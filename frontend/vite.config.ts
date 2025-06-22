17f5df43d21f3942f1ec9aa2c36e3735680e376f
Abdul Rehman Memon <amemon.bscs22seecs@seecs.edu.pk>
2025-06-22 22:50:48 +0500

    feat: Add @types/node dependency and update TypeScript configuration for improved type support

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,12 @@
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
+import { dirname, resolve } from 'path';
+
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [react(),tailwindcss()],
